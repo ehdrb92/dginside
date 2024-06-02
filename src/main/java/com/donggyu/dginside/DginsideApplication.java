8a42d949afdada9e27b4f6351634d1d59a236491
김동규 <gimdong-gyu@gimdong-gyuui-MacBookAir.local>
2024-06-02 20:37:18 +0900

    project init

---
+++ src/main/java/com/donggyu/dginside/DginsideApplication.java
@@ -0,0 +1,13 @@
+package com.donggyu.dginside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DginsideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DginsideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donggyu/dginside/DginsideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donggyu.dginside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DginsideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
