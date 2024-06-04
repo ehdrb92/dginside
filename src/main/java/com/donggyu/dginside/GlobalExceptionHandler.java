e0427bb1198c0b3d7c8393e1e17db0bcf217c1fc
donggyu <kcj920407@gmail.com>
2024-06-04 22:01:36 +0900

    add: Global 예외처리 클래스 추가

---
+++ src/main/java/com/donggyu/dginside/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.donggyu.dginside;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+        String errorMessage = "Data already exists.";
+        return new ResponseEntity<>(errorMessage, HttpStatus.CONFLICT);
+    }
+}
